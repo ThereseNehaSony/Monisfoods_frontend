dab3a3f3780dfbf5af97ef9034b01c6d52b90c41
Therese Neha Sony <nehakarickal100@gmail.com>
2024-11-27 22:04:25 +0530

    home page modified

--- src/pages/public/Home.jsx
+++ src/pages/public/Home.jsx
@@ -1,178 +1,139 @@
-/* eslint-disable no-unused-vars */
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import React from 'react'
+import { BiSearch } from "react-icons/bi";
+import BgImage from '../../assets/pic.jpg'
+import breakfast from '../../assets/breakfast.jpg'
+import lunch from '../../assets/lunchh.jpeg'
+import dinner from '../../assets/dinner.jpg'
+import logo from '../../assets/logo1.png'
 
-const ParentHomePage = () => {
-  const [students, setStudents] = useState([]); 
-  const [isAddStudentModalOpen, setIsAddStudentModalOpen] = useState(false);
-  const [schools, setSchools] = useState([]);
-  const [newStudent, setNewStudent] = useState({
-    name: '',
-    class: '',
-    school: '',
-  });
-  
-  const navigate = useNavigate(); 
-
-
-  useEffect(() => {
-    const fetchStudents = async () => {
-      try {
-        const response = await axios.get('https://monis-foods-backend.vercel.app/api/user/students');
-        setStudents(response.data);
-      } catch (error) {
-        console.error("Error fetching students:", error);
-      }
-    };
-
-    fetchStudents();
-  }, []);
- 
-  useEffect(() => {
-    const fetchSchools = async () => {
-      try {
-        const response = await axios.get("https://monis-foods-backend.vercel.app/api/admin/schools"); 
-        
-        setSchools(response.data.schools);
-      } catch (error) {
-        console.error("Error fetching schools:", error);
-      }
-    };
-  
-    fetchSchools(); 
-  }, [isAddStudentModalOpen]); 
-  
-
-  const handleBookMeals = (student) => {
-    navigate(`/menu`);
-  };
-
-  
-  const handleAddStudent = async () => {
-    try {
-      const response = await axios.post('https://monis-foods-backend.vercel.app/api/user/students', newStudent);
-      setStudents((prevStudents) => [...prevStudents, response.data]);
-      setNewStudent({ name: '', class: '', school: '' });
-      setIsAddStudentModalOpen(false);
-    } catch (error) {
-      console.error("Error adding student:", error);
-    }
-  };
-
-  
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setNewStudent((prevStudent) => ({
-      ...prevStudent,
-      [name]: value,
-    }));
-  };
-
+function Home() {
   return (
-    <div className="min-h-screen p-8 text-black">
-      <div className="text-center mb-8">
-        <h1 className="text-4xl font-bold mb-4">Welcome to Monis Foods</h1>
-        <p className="text-lg mb-4">
-          Here at our mess, we ensure your child receives healthy and nutritious meals every day.
-          Book meals for your child now and enjoy a hassle-free experience.
-        </p>
-        <button
-          onClick={() => setIsAddStudentModalOpen(true)}
-          className="bg-yellow-500 text-black py-2 px-6 rounded-full shadow-md hover:bg-yellow-400 transition duration-300"
-        >
-          Add a Student
-        </button>
-      </div>
 
-      <h2 className="text-2xl font-semibold mb-4">Your Students</h2>
-      {students.length === 0 ? (
-        <p className="text-center text-lg text-gray-300">{`You haven't added any students yet.`}</p>
-      ) : (
-        <ul className="space-y-4">
-          {students.map((student) => (
-            <li
-              key={student.id}
-              className="flex justify-between items-center bg-white text-black p-4 rounded-lg shadow-md"
-            >
-              <div>
-              <h3 className="text-xl font-semibold">{student.name.toUpperCase()}</h3>
 
-                <p className="text-sm">{student.class} - {student.school.toUpperCase()}</p>
+    <div>
+<div className="lg:h-screen bg-color lg:flex lg:items-center overflow-clip">
+            <div className="lg:w-1/2 flex-shrink-0 text-white pt-32 px-10 lg:pl-48 ">
+              <h1 className="text-4xl lg:text-6xl font-bold mb-6">
+               Monis Food: Authentic Flavors, Exceptional Service
+              </h1>
+              <p className="font-semibold text-gray-500 text-sm mb-5 lg:text-lg mb-10 lg:mb-">
+              At Monis Food, we believe that eating healthy should be easy and enjoyable for everyone.
+               That's why we are committed to delivering high-quality, nutritious meals directly to students, teachers, and parents.
+                Through our convenient service, Monis Food On Demand, we offer fresh, delicious meals every day for breakfast, lunch,
+                 and snacks, ensuring that our customers have access to wholesome food choices that fuel their busy lives.
+              </p>
+              <div className="flex justify-between rounded-2xl py-2 pl-2 lg:pl-5 pr-2 font-semibold">
+                {/* <div className="flex items-center lg:gap-3">
+                  <BiSearch className="text-2xl text-blue-600" />
+                  <input
+                    type="text"
+                    placeholder="Find the best product"
+                    className="text-black outline-none w-full"
+                  />
+                </div> */}
+                {/* <button className="btn-blue"
+                //  onClick={gotoLogin}
+                 >
+                  Search
+                </button> */}
               </div>
-              <button
-                onClick={() => handleBookMeals(student)}
-                className="bg-green-500 text-white py-2 px-4 rounded-full hover:bg-green-400 transition duration-300"
-              >
-                Book Meals
-              </button>
-            </li>
-          ))}
-        </ul>
-      )}
-
-      {/* Add Student Modal */}
-      {isAddStudentModalOpen && (
-        <div className="fixed inset-0 bg-gray-900 bg-opacity-75 flex items-center justify-center z-50">
-          <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full">
-            <h3 className="text-xl font-semibold mb-4">Add New Student</h3>
-            <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700 mb-1">Name</label>
-              <input
-                type="text"
-                name="name"
-                value={newStudent.name}
-                onChange={handleInputChange}
-                className="w-full p-2 border border-gray-300 rounded-md text-gray-700"
-              />
             </div>
-            <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700 mb-1">Class</label>
-              <input
-                type="text"
-                name="class"
-                value={newStudent.class}
-                onChange={handleInputChange}
-                className="w-full p-2 border border-gray-300 rounded-md text-gray-700"
-              />
-            </div>
-            <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-700 mb-1">School</label>
-            <select
-              name="school"
-              value={newStudent.school}
-              onChange={handleInputChange}
-              className="w-full p-2 border border-gray-300 rounded-md text-gray-700"
-            >
-              <option value="" disabled>
-                Select a school
-              </option>
-              {schools?.map((school) => (
-                <option key={school._id} value={school.name}>
-                  {school.name}
-                </option>
-              ))}
-            </select>
+          
+            {/* <img
+              src={BgImage}
+              className="lg:w-[1200px] lg:-ml-32 lg:mt-16"
+              alt="Bg Image"
+            /> */}
           </div>
-            <div className="flex justify-between">
-              <button
-                onClick={() => setIsAddStudentModalOpen(false)}
-                className="text-gray-600 hover:text-gray-800"
-              >
-                Cancel
-              </button>
-              <button
-                onClick={handleAddStudent}
-                className="bg-blue-500 text-white py-2 px-6 rounded-full hover:bg-blue-400 transition duration-300"
-              >
-                Add Student
-              </button>
+
+          <section className="wrapper mt-2">
+  <div className="container-fostrap">
+    <div className="mb-4"></div>
+    <div className="content">
+      <div className="container mx-auto">
+        <div className="flex flex-wrap -mx-2">
+          <div className="w-full sm:w-1/3 px-2 mb-4">
+            <div className="card bg-white shadow-md hover:shadow-xl rounded-3xl overflow-hidden">
+              <div className="img-card block overflow-hidden flex justify-center  items-center" >
+                <img src={breakfast} className="w-44 h-44 object-cover rounded-full transition-transform duration-300 hover:scale-110" />
+              </div>
+              <div className="card-content p-4 text-left">
+                <h4 className="card-title font-bold text-xl mb-2">
+                <div className="text-black no-underline text-center block">
+                 Breakfast
+                    </div>
+
+                </h4>
+                {/* <p className="text-gray-700">
+                  Tutorial to make a carousel bootstrap by adding more wonderful effect fadein ...
+                </p> */}
+              </div>
+              <div className="card-read-more border-t border-gray-200">
+              <p className="block text-center py-2 font-semibold uppercase text-black-600">
+        Book by 8 PM
+      </p>
+              </div>
+            </div>
+          </div>
+
+          {/* Second Card */}
+          <div className="w-full sm:w-1/3 px-2 mb-4">
+            <div className="card bg-white shadow-md hover:shadow-xl rounded-3xl overflow-hidden">
+              <div className="img-card block overflow-hidden flex justify-center items-center" >
+                <img src={lunch} className="w-44 h-44 object-cover rounded-full transition-transform duration-300 hover:scale-110" />
+              </div>
+              <div className="card-content p-4 text-left">
+                <h4 className="card-title font-bold text-xl mb-2">
+                  <div  className="text-black no-underline text-center block">
+                    Lunch
+                  </div>
+                </h4>
+                {/* <p className="text-gray-700">
+                  Material Design is a visual programming language made by Google...
+                </p> */}
+              </div>
+              <div className="card-read-more border-t border-gray-200">
+              <p className="block text-center py-2 font-semibold uppercase text-black-600">
+        Book by 8 PM
+      </p>
+              </div>
+            </div>
+          </div>
+
+          {/* Third Card */}
+          <div className="w-full sm:w-1/3 px-2 mb-4">
+            <div className="card bg-white shadow-md hover:shadow-xl rounded-3xl overflow-hidden">
+              <div className="img-card block overflow-hidden flex justify-center items-center" >
+                <img src={dinner} className="w-44 h-44 object-cover rounded-full transition-transform duration-300 hover:scale-110" />
+              </div>
+              <div className="card-content p-4 text-left">
+                <h4 className="card-title font-bold text-xl mb-2">
+                  <div className="text-black no-underline text-center block">
+                    Dinner
+                  </div>
+                </h4>
+                {/* <p className="text-gray-700">
+                  Tutorials button hover animation, although very much a hover button is very beautiful...
+                </p> */}
+              </div>
+              <div className="card-read-more border-t border-gray-200">
+              <p className="block text-center py-2 font-semibold uppercase text-black-600">
+        Book by 8 PM
+      </p>
+              </div>
             </div>
           </div>
         </div>
-      )}
+      </div>
     </div>
-  );
-};
+  </div>
+</section>
+<div className="bg-black flex justify-center items-center p-4">
+      <img src={logo}  alt="Logo" />
+    </div>
 
-export default ParentHomePage;
+    </div>
+  )
+}
+
+export default Home
